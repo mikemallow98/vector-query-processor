fb53d38d95cfbd869ea87d9227f7330466a1847a
Mike Mallow <herewegolakers@gmail.com>
2020-03-24 14:45:18 -0400

    updated header file with method names

--- src/terms.hpp
+++ src/terms.hpp
@@ -10,12 +10,16 @@ struct DictionaryEntry{
     std::string term;
     int document_freq;
     int offset;
+    double weight_idf;
+    double weight;
 };
 
 struct Posting{
     int document_id;
     int term_freq;
     double weight;
+    
+    double weight_tf;
 };
 
 class Terms{
@@ -26,12 +30,15 @@ class Terms{
         DictionaryEntry get_dictionary_entry(std::string term);
         Posting get_postings_entry(int offset);
         void calculate_term_weight();
+        void calculate_overall_weight();
+        void calculate_idf_weight();
 
     private:
         std::unordered_map<std::string, DictionaryEntry> dictionary;
         std::vector<Posting> postings;
         void update_dictionary(std::string filename);
         void update_postings(std::string filename);
+
 
 };
 
